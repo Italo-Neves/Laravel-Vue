5b0fe902e5164210dc073136889092be5f3611af
Italo-Neves <italorei7@gmail.com>
2024-01-22 22:56:10 -0300

    primeiro commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',[\App\Http\Controllers\PrincipalController::class,'principal'])->name('site.index');
+Route::get('/sobre-nos',[\App\Http\Controllers\SobreNos::class,'sobrenos'])->name('site.sobrenos');
+Route::get('/contato',[\App\Http\Controllers\Contato::class,'contato'])->name('site.contato');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/login',[\App\Http\Controllers\Login::class,'login'])->name('app.login');
+    Route::get('/clientes',[\App\Http\Controllers\Clientes::class,'Clientes'])->name('app.clientes');
+    Route::get('/fornecedores',[\App\Http\Controllers\FornecedorController::class,'Index'])->name('app.fornecedores');
+    Route::get('/produtos',[\App\Http\Controllers\Produtos::class,'produtos'])->name('app.produtos');
+});
+
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ser redirecionado a página principal.';
 });
